a91d2ce905aa1a09d0167be3c9a5b09a69da5c4b
Akira Kano <a.kano1101@gmail.com>
2021-02-27 16:18:12 +0900

    ✨導入

---
+++ install.sh
@@ -0,0 +1,20 @@
+git clone git@github.com:vaiorabbit/ruby-imgui.git
+
+cd ruby-imgui/
+git submodule update --init --recursive
+
+cd cimgui
+git pull origin master
+git submodule update
+cd ../
+
+cd third_party
+./glfw_build.sh
+cd ../
+
+cd imgui_dll
+./imgui_dll_build.sh
+cd ../
+
+cd sample
+ruby test_glfw_opengl2.rb
